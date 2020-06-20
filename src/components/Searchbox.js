57c38c15e08a5ba7b06f0d277343790b748db214
linchen5913 <bushigi5913@gmail.com>
2020-06-20 14:22:52 +0800

    added useRef to autoFocus the input

--- src/components/Searchbox.js
+++ src/components/Searchbox.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, {useRef, useEffect} from 'react';
 
 const Searchbox = ({ searchChange }) =>{
+    const inputRef = useRef()
+
+    useEffect(()=>{
+        inputRef.current.focus()
+    },[])
+
     return (
         <div className='pa2'>
             <input 
                 className='pa3 ba b--green bg-lightest-blue'
                 type='search' 
                 placeholder='search robot' 
-                onChange={searchChange}
+                onChange={searchChange} 
+                ref={inputRef}
             />
         </div>
     )
